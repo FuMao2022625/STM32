4d9a14be459a29f18d005650140e6fda67399a8c
T1.winds <153503409+FuMao2022625@users.noreply.github.com>
2026-06-10 21:03:46 +0800

    1

--- 《室内微型火灾预警系统》/APP/UltrasonicCtrol.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ULTRASONICCTROL_H_
-#define __ULTRASONICCTROL_H_
-
-extern volatile unsigned int distance_cm;//µ±«∞æ‡¿Î
-void UltraSoundInit(void);
-void GetDistanceDelay(void);
-void Distance(void);
-void LCD12864WriteDistance(unsigned int distance);
-#endif
